b311e2ec0a3c9a30c584031b59b48ff3bdf795ca
warmdev17 <warmdevofficial@gmail.com>
2024-04-19 14:38:24 +0700

    change theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={spaceGrotesk.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
