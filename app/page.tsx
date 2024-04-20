49dcb189fdd552d56f31dba61663d2ed08ed06c8
warmdev17 <warmdevofficial@gmail.com>
2024-04-20 19:30:42 +0700

    upate gui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Change from "./components/DijkstrasAlgorithm";
 import ShortestPath from "./components/ShortestPath";
 
 export default function Home() {
   return (
     <div className="items-center justify-center flex flex-col">
-      <ShortestPath />
+      {/* <ShortestPath /> */}
+      <Change />
     </div>
   );
 }
